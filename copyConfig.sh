c4175b8f9ab8b4680a632c274743c8c92cfd9c11
qph-manjaro <1258607043@qq.com>
2022-08-07 14:59:02 +0800

    2022-08-07 14:58

--- copyConfig.sh
+++ copyConfig.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 mkdir ./.config
-mkdir ./.config/nvim
-cp -r ~/.config/nvim/ ./.config/nvim
+cp -r ~/.config/nvim/ ./.config/
 cp -r ~/.config/fish/ ./.config/
 cp -r ~/.config/omf/ ./.config/
 cp -r ~/.config/alacritty/ ./.config/
 
 mkdir ./.local
 mkdir ./.local/share
-mkdir ./.local/share/omf
-cp -r ~/.local/share/omf/ ./.local/share/omf
-mkdir ./.local/share/fish
-cp -r ~/.local/share/fish/ ./.local/share/fish
-mkdir ./.local/share/nvim
-cp -r ~/.local/share/nvim/ ./.local/share/nvim
+cp -r ~/.local/share/omf/ ./.local/share/
+cp -r ~/.local/share/fish/ ./.local/share/
+cp -r ~/.local/share/nvim/ ./.local/share/
